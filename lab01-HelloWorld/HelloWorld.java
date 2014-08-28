aed60689a6a990ced24a8ad1b08a2ba257d89dc6
keb218 <keb218@lehigh.edu>
2014-08-28 20:38:17 +0000

    updated HelloWorld

--- lab01-HelloWorld/HelloWorld.java
+++ lab01-HelloWorld/HelloWorld.java
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////
-//Brian Chen
+//Kelli Barber
 //Demo 0
 //Hello World Java Program
 //
@@ -8,13 +8,13 @@
 //  run the program
 //      java HelloWorld//
 
-//define a class
+//  define a class
 public class HelloWorld{
 
 //  add main method
 public static void main(String[] args) {
 
-//print the statement with the bo around it
+//  print the statement with the bo around it
     System.out.println("#########################");
     System.out.println("#########################");
     System.out.println("#                       #");
