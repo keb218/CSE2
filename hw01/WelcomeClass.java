4f570a360903c9b0dcf1cd9c5507ea79d4c25b74
keb218 <keb218@lehigh.edu>
2014-08-29 02:44:01 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////
+//Kelli Barber
+//homework 1
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass {
+
+//  add main method
+public static void main(String[] args) {
+
+    //print the statement and shapes
+    System.out.println("  -------------- ");
+    System.out.println("  |   WELCOME  | "); 
+    System.out.println("  -------------- ");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\ /\\ /\\ /\\ /\\ /\\ ");
+    System.out.println("|-K--E--B--2--1--8-| ");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("This summer my friends and I went to Florida. Now I am studying at Lehigh!");
+    
+    }
+}
+
