f269b3ea263d51bd0e25f982fd7d3291020c873c
keb218 <keb218@lehigh.edu>
2014-09-13 19:52:06 +0000

    updated FourDigits

---
+++ hw03/FourDigits.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+///////////////////////////
+//Kelli Barber
+//hw03
+//FourDigits Program
+//9/13/14
+//
+// This program will ask the user to 
+//enter a double. It will then print
+// out the first four digits to the 
+//right of the decimal point // 
+
+public class FourDigits{
+    //main method neeeded for every program
+    public static void main(String[] args) {
+        Scanner myScanner; //name the scanner
+        myScanner = new Scanner (System.in); //define the scanner
+        //as a user for a double
+        System.out.print(
+            "Enter a double and I display the four digits to the right of the decimal point- ");
+            double x = myScanner.nextDouble(); //x is defined as the number the user enters
+            double x2 = (int)(x*10000); //x is multiplied by 10000 and casted to an int
+            int x3 = (int)(x2 % 10000); //x is divided by 10000 and the remainder shows
+           
+        //the last four digits are printed out    
+        System.out.println(
+            "The four digits are " + x3+ ".");
+    }
+}
