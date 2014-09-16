85c626623fa509f5033d96e3f3163b096e8bb9a3
keb218 <keb218@lehigh.edu>
2014-09-16 01:43:17 +0000

    updated Root

--- hw03/Root.java
+++ hw03/Root.java
@@ -21,16 +21,16 @@ public class Root {
             "Enter a double, and I will print its cube root- "); //user is told to enter a double
             double x = myScanner.nextDouble(); //the number user enters is stored as 'x'
             double guess = x/3; //guess is defined 
-            double guess0 = (guess*guess*guess+x)/(3*guess*guess); //guess begins to become more accurate
+            double guess0 = (2*guess*guess*guess+x)/(3*guess*guess); //guess begins to become more accurate
             double guess1 = (2*guess0*guess0*guess0+x)/(3*guess0*guess0); // each guess is slightly closer to the cube root
             double guess2 = (2*guess1*guess1*guess1+x)/(3*guess1*guess1); //guess 2 is closer still
             double guess3 = (2*guess2*guess2*guess2+x)/(3*guess2*guess2); //guess 3 is the closest so far
-           // double guess4 = (2*guess3*guess3*guess3+x)/(3*guess3*guess3);
-            double crudeAnswer = guess3*guess3*guess3; //the number most accurate is defined as crude answer
+            double guess4 = (2*guess3*guess3*guess3+x)/(3*guess3*guess3);
+            double crudeAnswer = guess4*guess4*guess4; //the number most accurate is defined as crude answer
         //print out the cube root and the close guess of the crude when cubed    
         System.out.println(
-            "The cube root is " + guess3 + ":"
-            + guess3 + "*" +guess3+ "*" +guess3 + " = "
+            "The cube root is " + guess4 + ":"
+            + guess4 + "*" +guess4+ "*" +guess4 + " = "
             + crudeAnswer+ ".");
             
             
